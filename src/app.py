4220256ed8e1822da760044d9d8c2bb9febdc773
Gokilapriya S <118598892+GokilapriyaSivakumar@users.noreply.github.com>
2024-04-22 21:58:42 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -6,8 +6,8 @@ from langchain_community.utilities import SQLDatabase
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 from langchain_groq import ChatGroq
-import streamlit as st
 
+import streamlit as st
 def init_database(user: str, password: str, host: str, port: str, database: str) -> SQLDatabase:
   db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
   return SQLDatabase.from_uri(db_uri)
